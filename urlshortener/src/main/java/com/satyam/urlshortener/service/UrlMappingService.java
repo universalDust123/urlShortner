d68461e63ecce586eb5f55e51b60560b580dd591
universalDust123 <satyam73233@gmail.com>
2026-02-14 18:35:30 +0530

    Optimized Redirecting url through redis

--- urlshortener/src/main/java/com/satyam/urlshortener/service/UrlMappingService.java
+++ urlshortener/src/main/java/com/satyam/urlshortener/service/UrlMappingService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.CachePut;
+// import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.CacheEvict;
 
 import java.time.LocalDateTime;
